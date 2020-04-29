dcf2233d62e6e4b8f315a31cfe80bb223e3f5322
Henry Liu <hl3184@columbia.edu>
2020-04-29 12:07:19 -0400

    Level 3 Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -15,9 +15,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = mongoose.model("User",userSchema);
 
@@ -40,7 +37,7 @@ app.route("/register")
 .post(function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -58,7 +55,7 @@ app.route("/login")
 })
 .post(function(req,res){
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: userName},function(err,foundUser){
     if (err){
       console.log(err);
